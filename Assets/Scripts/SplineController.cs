b3b09dde3069a36f077f5ad42f092aac95ce23a4
kaptainKirby <rherwig@mail.bradley.edu>
2024-12-13 21:52:44 -0600

    skeleton finished

--- Assets/Scripts/SplineController.cs
+++ Assets/Scripts/SplineController.cs
@@ -1,30 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Splines;
 
 public class SplineController : MonoBehaviour
 {
     [SerializeField] private int splineStartIndex;
+    private int currentSplineIndex;
     private SplineAnimate[] splinePath;
-
     private void Start()
     {
+        currentSplineIndex = splineStartIndex;
+        //Subscribes event to method
+        SplineAnimate.ReachedEndOfSpline += SwitchSpline;
+
+        //Gets all spline scripts on cart
         splinePath = gameObject.GetComponentsInChildren<SplineAnimate>();
 
+        SetSplineTrack();
+    }
+
+    private void SwitchSpline()
+    {
+        currentSplineIndex++;
+
+        SetSplineTrack();
+    }
+
+    private void SetSplineTrack()
+    {
+        //Checks for error
+        if (currentSplineIndex >= splinePath.Length)
+            return;
+
+        //Sets all spline scripts to be disabled except for starting spline
         for (int i = 0; i < splinePath.Length; i++)
         {
-            if (i == splineStartIndex)
+            if (i == currentSplineIndex)
                 splinePath[i].enabled = true;
             else
                 splinePath[i].enabled = false;
         }
-
-        SplineAnimate.
     }
 
     private void OnDisable()
     {
-
+        SplineAnimate.ReachedEndOfSpline -= SwitchSpline;
     }
 }
