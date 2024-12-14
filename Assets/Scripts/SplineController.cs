22939848dafad8cb5516c74d55546b3b9e4a2a23
kaptainKirby <rherwig@mail.bradley.edu>
2024-12-13 19:17:45 -0600

    p

--- Assets/Scripts/SplineController.cs
+++ Assets/Scripts/SplineController.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class SplineController : MonoBehaviour
 {
+    [SerializeField] private int splineStartIndex;
+    private SplineAnimate[] splinePath;
 
+    private void Start()
+    {
+        splinePath = gameObject.GetComponentsInChildren<SplineAnimate>();
+
+        for (int i = 0; i < splinePath.Length; i++)
+        {
+            if (i == splineStartIndex)
+                splinePath[i].enabled = true;
+            else
+                splinePath[i].enabled = false;
+        }
+
+        SplineAnimate.
+    }
+
+    private void OnDisable()
+    {
+
+    }
 }
