b24cd3027baaef7e9511ebcb1e30e7a776f09e58
cgdugal <cdugal@mail.bradley.edu>
2025-04-03 15:15:00 -0500

    Shovel reconnect at a consistent orientation

--- Assets/Scripts/ShovelReconnect.cs
+++ Assets/Scripts/ShovelReconnect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShovelReconnect : MonoBehaviour
 {
     [SerializeField] private GameObject connectedShovel;
+    [SerializeField] private Vector3 shovelOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,14 @@ public class ShovelReconnect : MonoBehaviour
     void OnEnable()
     {
         connectedShovel.SetActive(false);
+        connectedShovel.transform.position = this.transform.position;
+        connectedShovel.transform.rotation = this.transform.rotation;
+        
+        connectedShovel.transform.rotation = Quaternion.Euler(
+            this.transform.rotation.eulerAngles.x + shovelOffset.x, 
+            this.transform.rotation.eulerAngles.y + shovelOffset.y, 
+            this.transform.rotation.eulerAngles.z + shovelOffset.z);
+        
         connectedShovel.SetActive(true);
     }
 }
